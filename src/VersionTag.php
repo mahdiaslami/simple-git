3616237aae5cac7477d5424165631228b99e9b56
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-04-09 10:56:38 +0430

    feat: add latest to VersionTag

--- src/VersionTag.php
+++ src/VersionTag.php
@@ -11,6 +11,18 @@ class VersionTag
         $this->repository = $repository;
     }
 
+    public function latest(): string
+    {
+        $v = $this->list();
+
+        uasort(
+            $v,
+            fn($a, $b) => version_compare($b, $a)
+        );
+
+        return array_values($v)[0];
+    }
+
     public function list(): array
     {
         $result = array_filter(
--- tests/VersionTest.php
+++ tests/VersionTest.php
@@ -25,4 +25,23 @@ class VersionTest extends TestCase
 
         $this->assertEquals(['v0.1.0', 'v0.2.0'], $repository->versionTags());
     }
+
+
+    public function test_latest_method()
+    {
+        Process::create($this->tempPath('r'))
+            ->add(['git', 'init'])
+            ->add(['git', 'config', '--local', 'user.email', "test@example.com"])
+            ->add(['git', 'config', '--local', 'user.name', "Test"])
+            ->add(['touch', 'file1'])
+            ->add(['git', 'add', '.'])
+            ->add(['git', 'commit', '-m', 'add file1'])
+            ->add(['git', 'tag', 'v0.1.0'])
+            ->add(['git', 'tag', 'v0.2.0'])
+            ->runAll();
+
+        $repository = new Repository($this->tempPath('r'));
+
+        $this->assertEquals('v0.2.0', $repository->versionTag()->latest());
+    }
 }
